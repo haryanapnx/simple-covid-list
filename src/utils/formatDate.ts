5565e8e6c44d69f25fbd0bb9cae853e8a25b7592
Haryana <hary.vai@gmail.com>
2021-03-08 23:37:30 +0700

    patch: changes constant variable

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,9 +1,8 @@
 export const formatDate = (dates: Date) => {
-  var date = new Date(dates);
+  const date = new Date(dates);
   // Create a list of names for the months
-  var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
   // return a formatted date
   return date.getDate() + ' ' + months[date.getMonth()] + ' ' + date.getFullYear();
-
 };
